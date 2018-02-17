173b79f863d07b8e60f87e2c1a49be0aafbf66e6
Anthony Garcia <anthony.garcia@digicatapult.org.uk>
2018-02-17 18:08:12 +0000

    Implement soql template

--- src/soql.js
+++ src/soql.js
@@ -1,1 +1,21 @@
-module.exports = String.raw
+const escape = require('./escape')
+
+function soql(strings, ...values) {
+  if (strings == null || strings.length === 0) {
+    return ''
+  }
+
+  if (values == null || values.length === 0) {
+    return strings[0]
+  }
+
+  const query = [strings[0]]
+  values.forEach((val, i) => {
+    query.push(escape(val))
+    query.push(strings[i + 1])
+  })
+
+  return query.join('')
+}
+
+module.exports = soql
